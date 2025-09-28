da5aadb87c4912167414e6102b6bcc011d04b484
Hansika Arora <hansikaarora3053@gmail.com>
2025-09-28 16:28:43 +0530

    updated index.js in backend

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const items = require('./itemsData');
 const users = require('./UserData');  
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
